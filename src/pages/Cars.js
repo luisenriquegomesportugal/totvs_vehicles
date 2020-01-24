f6c2a768538be5f4fe8abf6c2938c343e16f1364
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-01-24 16:51:02 -0300

    Edit design in Cars

--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -1,135 +1,138 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-  TouchableOpacity,
-  Text,
-  Image
-} from "react-native";
-import carImg from "../assets/car.png";
-import favorite from "../assets/star.png";
-import favoriteOn from "../assets/starOn.png";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
+import carImg from '../assets/profile.svg';
+import favorite from '../assets/star.png';
+import favoriteOn from '../assets/starOn.png';
 
-function Cars() {
-  const [CarName, setCarName] = useState("");
-  const [year, setYear] = useState("");
-  const [km, setKm] = useState("");
-  const [price, setPrice] = useState("");
+function Cars(props) {
   const [favor, setFavor] = useState(false);
-  const [Descripiton, setDescription] = useState("");
-
   const [favoriteImg, setFavoriteImg] = useState(favorite);
 
   function changeFavorite() {
-    if (favor === false) {
-      setFavoriteImg(favoriteOn);
-      setFavor(true);
-    } else {
-      setFavoriteImg(favorite);
-      setFavor(false);
-    }
+    const img = favor ? favoriteOn : favorite;
+
+    setFavoriteImg(img);
+    setFavor(!favor);
   }
 
   const car = {
-    manufacturer: "ford",
-    name: "ka",
-    year: "1009",
+    manufacturer: 'Ford',
+    name: 'KA',
+    year: '2016',
     km: 2000,
     price: 48000,
-    seller: "jhon silva",
+    seller: 'Jhon Silva',
     favor: false,
-    Descripiton: "launch editon",
+    Description: 'launch editon',
     optional: [
       {
-        icon: "image/icon",
-        label: "motor 1.0"
-      }
-    ]
+        icon: 'image/icon',
+        label: 'motor 1.0',
+      },
+    ],
   };
 
-  useEffect(() => {}, [
-    //testes
-  ]);
+  useEffect(() => {
+    changeFavorite();
+  }, []);
 
   return (
-      <View style={styles.container}>
-        <View>
-          <TouchableOpacity
-              style={styles.imgContainer}
-              onPress={() => {}}
-              style={styles.imgContainer}
-          >
-            <Image source={carImg} style={styles.profileLogo} />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={changeFavorite} style={styles.fav}>
-            <Image source={favoriteImg} style={styles.favLogo} />
-          </TouchableOpacity>
-
-          <View style={styles.cardInfo}>
-            <Text style={styles.cardInfoText}>Fabricado:{car.manufacturer}</Text>
-            <Text style={styles.cardInfoText}>Modelo:{car.name}</Text>
-            <Text style={styles.cardInfoText}>Ano:{car.year}</Text>
-            <Text style={styles.cardInfoText}>valor:{car.price}</Text>
-            <Text style={styles.cardInfoText}>Vendedor:{car.seller}</Text>
-            <Text style={styles.cardInfoText}>opcionais:</Text>
-            <Text style={styles.cardInfoText}>Descrição:{car.Descripiton}</Text>
-          </View>
-        </View>
+    <View style={styles.container}>
+      
+      <View style={styles.imgContainer}>
+        <Image source={carImg} style={styles.profileLogo} />
       </View>
+
+      <TouchableOpacity onPress={changeFavorite} style={styles.fav}>
+        <Image source={favoriteImg} style={styles.favLogo} />
+      </TouchableOpacity>
+
+      <View style={styles.cardInfo}>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Fabricante: </Text>
+          {car.manufacturer}
+        </Text>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Modelo: </Text>
+          {car.name}
+        </Text>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Ano: </Text>
+          {car.year}
+        </Text>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Preço: </Text>
+          R$ {car.price}
+        </Text>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Vendedor: </Text>
+          {car.seller}
+        </Text>
+        <Text style={styles.cardInfoBox}>
+          <Text style={styles.cardInfoText}>Descrição: </Text>
+          {car.Description}
+        </Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f1f1f1",
-    color: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#f1f1f1',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
   },
   imgContainer: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    marginHorizontal: 30,
-    alignItems: "center",
-    paddingTop: 30,
-    justifyContent: "center",
-    backgroundColor: "#3B83Bd"
+    width: 150,
+    height: 150,
+    borderRadius: 80,
+    marginHorizontal: '20%',
+    alignItems: 'center',
+    paddingTop: 0,
+    justifyContent: 'center',
+    backgroundColor: '#3B83Bd',
   },
   profileLogo: {
-    width: 200,
-    height: 200
+    width: 140,
+    height: 140,
+    borderRadius: 80,
   },
   favLogo: {
     width: 25,
-    height: 25
+    height: 25,
   },
   fav: {
-    width: 50,
-    height: 10,
     borderRadius: 10,
-    marginLeft: 15,
-    alignItems: "center",
-    paddingVertical: 20,
-    marginBottom: 40,
-    justifyContent: "center",
-    backgroundColor: "#ccc"
+    alignSelf: 'center',
+    alignItems: 'center',
+    padding: 15,
+    marginVertical: 10,
+    justifyContent: 'center',
+    backgroundColor: '#ccc',
   },
   cardInfo: {
-    justifyContent: "center",
+    width: '70%',
+    marginHorizontal: 0,
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     borderRadius: 5,
-    backgroundColor: "#bfcade"
+  },
+  cardInfoBox: {
+    width: '100%',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#999',
+    fontSize: 17,
   },
   cardInfoText: {
-    paddingVertical: 10,
-    fontSize: 22,
-    justifyContent: "center",
-    paddingHorizontal: 5,
-    color: "rgb(32, 39, 47)"
-  }
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
 });
 
 export default Cars;
